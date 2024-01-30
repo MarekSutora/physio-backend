bfe5ddc016549fd75a794844f59a1a490bb6158f
Marek <marek.sutora@gmail.com>
2024-01-30 15:21:51 +0100

    test controller added

---
+++ API/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace diploma_thesis_backend.Controllers
+{
+    public class TestController : Controller
+    {
+
+        //test endpoint which returns string
+        [HttpGet]
+        [Route("api/test")]
+        public IActionResult Test()
+        {
+            return Ok("Test");
+        }
+    }
+}
