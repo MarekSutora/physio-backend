ea5d1e25c5b53001ac47f3824fe315872ff69106
Marek <marek.sutora@gmail.com>
2024-04-15 18:53:26 +0200

    removed unused files

--- Domain/Entities/Abstract/IApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace diploma_thesis_backend.Models.Domain
-{
-    public interface IApplicationUser
-    {
-        string? Name { get; set; }
-        DateTime DateCreated { get; set; }
-    }
-}
